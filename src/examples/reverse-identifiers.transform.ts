689c8577c613709cb0c44c9fab6a9263fe1b30dd
Cheong Yip <wingy3181@gmail.com>
2020-03-15 13:43:34 +1100

    feat: add example 'reverse-identifiers' transform

---
+++ src/examples/reverse-identifiers.transform.ts
@@ -0,0 +1,19 @@
+import { API, FileInfo, JSCodeshift, Transform } from "jscodeshift/src/core";
+
+const transform: Transform = (file: FileInfo, api: API) => {
+    const j: JSCodeshift = api.jscodeshift;
+    return j(file.source)
+        .find(j.Identifier)
+        .replaceWith(
+            p => {
+                return {
+                    ...p.node,
+                    name: p.node.name.split('').reverse().join('')
+                };
+            }
+        )
+        .toSource();
+};
+
+export default transform;
+export const parser = 'flow'; // babel|babylon|flow|ts|tsx (See https://github.com/facebook/jscodeshift#usage-cli)
