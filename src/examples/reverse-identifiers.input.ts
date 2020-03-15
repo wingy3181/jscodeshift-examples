ad27bdacc4820a200407999029e5b810d7eb1e02
Cheong Yip <wingy3181@gmail.com>
2020-03-15 14:44:03 +1100

    chore: re-format code after editorconfig

--- src/examples/reverse-identifiers.input.ts
+++ src/examples/reverse-identifiers.input.ts
@@ -1,11 +1,11 @@
 type NameOrNameArray = string | string[];
 
 function createName(name: NameOrNameArray) {
-    if (typeof name === "string") {
-        return name;
-    } else {
-        return name.join(" ");
-    }
+  if (typeof name === "string") {
+    return name;
+  } else {
+    return name.join(" ");
+  }
 }
 
 var greetingMessage = `Greetings, ${createName(["Sam", "Smith"])}`;
--- src/examples/reverse-identifiers.transform.ts
+++ src/examples/reverse-identifiers.transform.ts
@@ -1,18 +1,18 @@
 import { API, FileInfo, JSCodeshift, Transform } from "jscodeshift/src/core";
 
 const transform: Transform = (file: FileInfo, api: API) => {
-    const j: JSCodeshift = api.jscodeshift;
-    return j(file.source)
-        .find(j.Identifier)
-        .replaceWith(
-            p => {
-                return {
-                    ...p.node,
-                    name: p.node.name.split('').reverse().join('')
-                };
-            }
-        )
-        .toSource();
+  const j: JSCodeshift = api.jscodeshift;
+  return j(file.source)
+    .find(j.Identifier)
+    .replaceWith(
+      p => {
+        return {
+          ...p.node,
+          name: p.node.name.split('').reverse().join('')
+        };
+      }
+    )
+    .toSource();
 };
 
 export default transform;
