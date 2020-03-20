455f3348f6f560888cd17bbb823861297182396a
Cheong Yip <wingy3181@gmail.com>
2020-03-20 20:01:26 +1100

    chore: add lint as part of ci

--- src/examples/reverse-identifiers/__testfixtures__/typescript/reverse-identifiers.output.ts
+++ src/examples/reverse-identifiers/__testfixtures__/typescript/reverse-identifiers.output.ts
@@ -2,4 +2,5 @@ const droWtsrif = "Hello ";
 const droWdnoces = "world";
 const egassem = droWtsrif + droWdnoces;
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const egasseMteg = (): string => egassem;
