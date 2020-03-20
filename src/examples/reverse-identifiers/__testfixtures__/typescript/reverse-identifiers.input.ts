d7201efc4088d642f24b6b1f4b7e147f1cc46eb8
Cheong Yip <wingy3181@gmail.com>
2020-03-20 20:01:26 +1100

    chore: add lint as part of ci

--- src/examples/reverse-identifiers/__testfixtures__/typescript/reverse-identifiers.input.ts
+++ src/examples/reverse-identifiers/__testfixtures__/typescript/reverse-identifiers.input.ts
@@ -2,4 +2,5 @@ const firstWord = "Hello ";
 const secondWord = "world";
 const message = firstWord + secondWord;
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const getMessage = (): string => message;
