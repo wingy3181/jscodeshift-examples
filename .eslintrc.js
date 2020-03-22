8af516587f84941162ca96e946084e7d999300d1
Cheong Yip <wingy3181@gmail.com>
2020-03-22 14:14:59 +1100

    chore: separate eslint for js and ts via overrides property

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,7 +25,6 @@
 //   - https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/README.md#supported-rules
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
   env: {
     node: true
   },
@@ -35,9 +34,6 @@ module.exports = {
   ],
   extends: [
     "eslint:recommended", // is ESLint's inbuilt "recommended" config - it turns on a small, sensible set of rules which lint for well-known best-practices.
-    "plugin:@typescript-eslint/eslint-recommended", // [@typescript-eslint/eslint-plugin] is a configuration we provide which disables a few of the recommended rules from the previous set that we know are already covered by TypeScript's typechecker.
-    "plugin:@typescript-eslint/recommended", // [@typescript-eslint/eslint-plugin] Uses the recommended rules from  @typescript-eslint/eslint-plugin
-    "prettier/@typescript-eslint", //  [eslint-config-prettier] Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     "plugin:prettier/recommended" // [eslint-plugin-prettier] Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   parserOptions: {
@@ -47,5 +43,22 @@ module.exports = {
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-  }
+  },
+  overrides: [
+    {
+      files: ["**/*.ts"],
+      parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+      extends: [
+        "eslint:recommended", // is ESLint's inbuilt "recommended" config - it turns on a small, sensible set of rules which lint for well-known best-practices.
+        "plugin:@typescript-eslint/eslint-recommended", // [@typescript-eslint/eslint-plugin] is a configuration we provide which disables a few of the recommended rules from the previous set that we know are already covered by TypeScript's typechecker.
+        "plugin:@typescript-eslint/recommended", // [@typescript-eslint/eslint-plugin] Uses the recommended rules from  @typescript-eslint/eslint-plugin
+        "prettier/@typescript-eslint", //  [eslint-config-prettier] Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+        "plugin:prettier/recommended" // [eslint-plugin-prettier] Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+      ],
+      rules: {
+        // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+        // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+      },
+    }
+  ]
 };
